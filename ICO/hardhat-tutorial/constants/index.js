16ca83975fefe653decdb9b4f13bcf885a951334
vijaykumarvj97 <emmanuelvijaykumar007@gmail.com>
2022-09-08 15:14:13 +0530

    upload ico token

---
+++ ICO/hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x339EC62A542a6aEbfC66847ad9613e4A41c3F5e3";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
